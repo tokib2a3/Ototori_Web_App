4fd6888ee15194e1eff7a1fbb269a942cc626e7d
tokib2a3 <147452699+tokib2a3@users.noreply.github.com>
2023-04-30 11:06:34 +0900

    v0.2.0

--- script.js
+++ script.js
@@ -1,76 +1,72 @@
-playing = false;
+var audioElements = document.getElementsByTagName('audio');
+var numAudioElements = audioElements.length;
+var audioBuffers = [];
+var audioContext;
+var playingCount = 0;
 
-timebar = document.querySelector("input[name=time]");
-curtime = document.querySelector(".current-time");
-tottime = document.querySelector(".total-time");
-firstaudio = document.querySelector("audio");
+function playAudio() {
+  audioContext = new (window.AudioContext || window.webkitAudioContext)();
+  var gainNode = audioContext.createGain();
+  gainNode.connect(audioContext.destination);
 
-setInterval(() => {
-  timebar.max = firstaudio.duration;
-  tottime.innerText = formatTime(firstaudio.duration);
-  if (document.activeElement != timebar) {
-    timebar.value = firstaudio.currentTime;
-    curtime.innerText = formatTime(firstaudio.currentTime);
-  }
-}, 200);
+  for (var i = 0; i < numAudioElements; i++) {
+    (function(i) { // 関数内で変数を定義
+      var request = new XMLHttpRequest();
+      request.open('GET', audioElements[i].src, true);
+      request.responseType = 'arraybuffer';
 
-function formatTime(sec) {
-  return Math.floor(sec / 60) + ":" + String(Math.floor(sec % 60)).padStart(2, "0")
-}
-
-timebar.oninput = () => {
-  curtime.innerText = formatTime(timebar.value);
-}
-
-timebar.onchange = () => {
-  if (playing) {
-    pause();
-    playing = true;
-  }
-  document.querySelectorAll("audio").forEach((e) => {
-    e.currentTime = timebar.value;
-  });
-  timebar.blur();
-  if (playing) {
-    play();
+      request.onload = function() {
+        audioContext.decodeAudioData(request.response, function(buffer) {
+          audioBuffers.push(buffer);
+          if (audioBuffers.length == numAudioElements) {
+            playBuffers();
+          }
+        });
+      };
+      request.send();
+    })(i); // 関数を即時実行
   }
 }
 
-function waitForLoad() {
-  return new Promise(resolve => {
-    const checkIfReady = () => {
-      readyStateSum = 0;
-      document.querySelectorAll("audio").forEach((e) => {
-        readyStateSum += e.readyState;
-      });
-      if (readyStateSum == document.querySelectorAll("audio").length * 4) {
-        resolve();
-      } else {
-        setTimeout(checkIfReady, 200);
+function playBuffers() {
+  var startTime = audioContext.currentTime + 0.1;
+  var sourceNodes = [];
+
+  for (var i = 0; i < numAudioElements; i++) {
+    var sourceNode = audioContext.createBufferSource();
+    sourceNode.buffer = audioBuffers[i];
+    sourceNode.connect(audioContext.destination);
+    sourceNode.start(startTime);
+    sourceNodes.push(sourceNode);
+  }
+
+  var checkInterval = setInterval(function() {
+    var minTime = audioContext.currentTime + 10;
+    var maxTime = audioContext.currentTime + 11;
+    var allSourcesArePlaying = true;
+
+    for (var i = 0; i < numAudioElements; i++) {
+      var sourceNode = sourceNodes[i];
+      var sourceNodeStartTime = sourceNode.startTime + audioContext.currentTime;
+      var sourceNodeEndTime = sourceNodeStartTime + sourceNode.buffer.duration;
+
+      if (sourceNodeEndTime < minTime) {
+        sourceNode.stop();
+        sourceNode.start(maxTime);
       }
-    };
-    checkIfReady();
-  });
-}
 
-// 音声データのロード
-document.querySelectorAll("audio").forEach((e) => {
-  e.load();
-});
+      if (sourceNode.startTime <= audioContext.currentTime) {
+        if (sourceNodeEndTime < audioContext.currentTime) {
+          sourceNode.stop();
+          sourceNode.start(maxTime);
+        }
+      } else {
+        allSourcesArePlaying = false;
+      }
+    }
 
-function play() {
-  waitForLoad().then(() => {
-    playing = true;
-    document.querySelectorAll("audio").forEach((e) => {
-      e.currentTime = timebar.value;
-      e.play();
-    });
-  });
-}
-
-function pause() {
-  playing = false;
-  document.querySelectorAll("audio").forEach((e) => {
-    e.pause();
-  });
+    if (allSourcesArePlaying) {
+      clearInterval(checkInterval);
+    }
+  }, 100);
 }
