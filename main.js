932ddc63978bc7eb06a4dc88b6abd3e3f886d0c3
tokib2a3 <147452699+tokib2a3@users.noreply.github.com>
2023-12-31 19:01:20 +0900

    v3.1.0

--- main.js
+++ main.js
@@ -489,7 +489,7 @@ displayVersion();
 
 // バージョン表示
 function displayVersion() {
-  const version = "3.0.4";
+  const version = "3.1.0";
   const versionElement = document.createElement("a");
   versionElement.id = "version";
   versionElement.href = "/ototori/changelog";
